fb0f553c3cf0bee99c04b91a7ba0a0151eb8379b
dylanmatthews <dylan.matthews@gmail.com>
2014-04-15 13:55:23 -0400

    removed canistreamit functionality

--- README.md
+++ README.md
@@ -1,26 +1,12 @@
 # Moviecutter
-					
+				
+## UPDATE - the streaming data is no longer available. The app is mostly useful for comparing Metacritic and Rotten Tomatoes scores to each other.
+	
 ### What is it?
 
-Moviecutter is a simple Python script that uses scraper APIs — created using [Kimono](http://kimonolabs.com) and Niels Lemmens's Python implementation of the [Can I Stream It? API](https://github.com/Bulv1ne/CanIStreamIt) — to compile streaming options for every movie which (a) received an 81 or higher on [Metacritic](http://www.metacritic.com/browse/movies/score/metascore/all?sort=desc&page=0) (b) is in the [Rotten Tomatoes Top 100 list](http://www.rottentomatoes.com/top/bestofrt/?category=0) or (c) is in the [Movie Review Query Engine (MRQE) top 100](http://www.mrqe.com/lists/100-best-films/mrqes-100-best-ranked-films). It then outputs a list of all qualifying movies, along with the relevant review aggregator score and streaming availability of each, into a CSV file for easy spreadsheet manipulation.
-
-### Dependencies
-
-Moviecutter requires Niels Lemmens's CanIStreamIt package, which you can install using pip:
-
-> pip install CanIStreamIt
-
-If you do not have pip, [download it](https://raw.github.com/pypa/pip/master/contrib/get-pip.py) and run:
-
-> python get-pip.py
-
-Then run pip to install CanIStreamIt.
+Moviecutter is a simple Python script that combines Metacritic and Rotten Tomatoes scores.
 
 ### Usage
-
-Once CanIStreamIt is installed, clone or download the Moviecutter project directory. In a terminal window, open the directory. Open the project directory and run moviecutter.py:
-
-> python moviecutter.py
 
 By default, the CSV file will be named "moviecutter.csv" and be placed in the Downloads folder of the user's home directory. Edit the value of the variable "writepath" at the top of the script if you would like it to write elsewhere.
 
--- moviecutter.py
+++ moviecutter.py
@@ -1,4 +1,3 @@
-import canistreamit
 import json
 import requests
 import csv
@@ -13,10 +12,10 @@ writepath = expanduser('~/Downloads/moviecutter.csv')
 
 # streaming services to check
 # key names are same as CanIStreamIt? for easier iteration
-services = ['netflix_instant', 'epix', 'crackle', 'redboxinstant_subscription',
-			'amazon_prime_instant_video', 'hulu_movies', 'hitbliss_streaming',
-			'youtube_free', 'snagfilms', 'streampix', 'hbo', 'showtime', 'cinemax',
-			'starz', 'encore', 'xfinity_free']
+# services = ['netflix_instant', 'epix', 'crackle', 'redboxinstant_subscription',
+#			'amazon_prime_instant_video', 'hulu_movies', 'hitbliss_streaming',
+#			'youtube_free', 'snagfilms', 'streampix', 'hbo', 'showtime', 'cinemax',
+#			'starz', 'encore', 'xfinity_free']
 
 # simple function for cleaning up titles by removing parens
 def cleantitle(title):
@@ -36,68 +35,53 @@ def cleantitle(title):
 r = requests.get('http://www.kimonolabs.com/api/7oxdwnlm?apikey=8df3757b504a8513ce1380c13b356cd9')
 metacritic = r.json()["results"]["collection1"]
 
-print "Metascores received\n" + str(datetime.now().time())
+print "Metascores received\n" + str(datetime.now().time())	
+print "Checking tomatometer scores and streaming services; this could take a couple of minutes..."
 
-# structure as a title : score dict
-meta = {}
-	
+headers = ["name", "metascore", "tomatometer"]
+csvlist = [headers]
+
+
 for (index, film) in enumerate(metacritic):
-	meta[cleantitle(film["title"]["text"])] = int(film["metascore"])
+	score = int(film["metascore"])
+	
+	# limit to 80+ scorers
+	if score>=80:
+	
+		
+		title = cleantitle(film["title"]["text"])
+		row = [title]
+		row.append(int(film["metascore"]))
+		
+		# get tomatometer and canistreamit data
+		try:
+			tomatometer = requests.get('http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=t33zpxft4pdupfnqnq4mbjsb&q='
+										+ title + '&page_limit=1').json()["movies"][0]["ratings"]["critics_score"]
+			
+			row.append(tomatometer)
+			
+		##	k = requests.get('http://www.canistream.it/services/search', params={'movieName': title}).content
+			
+		##	movie = canistreamit.search(title)[0]['_id']
+		##	print movie
+		##	for service in services:
+		##		try:
+		##			if service in canistreamit.streaming(movie):
+		##				row.append("True")
+		##			elif service in canistreamit.xfinity(movie):
+		##				row.append("True")
+		##			else:
+		##				row.append("False")
+		##		except requests.exceptions.ConnectionError:
+		##			continue
 
-# get rid of under 80 scorers
-meta2 = {}
-for i in meta.keys():
-	if meta[i] >= 80:
-		meta2[i] = meta[i]
-meta = meta2
-
-
-# find tomatometer scores for Metacritic/MRQE picks
-for k in meta.iterkeys():
-	try:
-		tomatometer = requests.get('http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=t33zpxft4pdupfnqnq4mbjsb&q='
-									+ str(k) + '&page_limit=1').json()["movies"][0]["ratings"]["critics_score"]
-		if not "tomatometer" in merged[k]:
-			merged[k]["tomatometer"] = tomatometer		
-	except:
-		continue
+		except:
+			continue
+				
+		csvlist.append(row)
 
 print "Tomatometers received\n" + str(datetime.now().time())
 
-print "Checking streaming services; this could take a couple of hours…"
-
-# connect to CanIStreamIt
-for i in merged.iterkeys():
-	try:
-		movie = canistreamit.search(i)[0]['_id']
-	except:
-		continue
-		
-	for service in services:
-		try:
-			if service in canistreamit.streaming(movie):
-				merged[i][service] = True
-			elif service in canistreamit.xfinity(movie):
-				merged[i][service] = True
-			else:
-				merged[i][service] = False
-		except requests.exceptions.ConnectionError:
-			continue
-
-print "Stream info received\n" + str(datetime.now().time())
-
-# format for CSV
-first = ["name", "metascore", "tomatometer"]
-headers = first+services
-csvlist = [headers]
-for i in merged.iterkeys():
-	row = [i]
-	for var in headers:
-		if var in merged[i]:
-			row.append(merged[i][var])	
-		else:
-			row.append("")
-	csvlist.append(row)
 	
 # write it out
 with open(writepath, 'wb') as csvfile:
